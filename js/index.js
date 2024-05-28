b012356c08dd19cb24e06da38eac10961856d112
Bharat Varshneya <bharatvarshneya99@gmail.com>
2024-05-28 22:06:15 +0530

    index.js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,30 @@
 import { generateAuthParams, addToFavorites } from './common.js';
 
-let allCharacters = [];
-
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search');
     const resultsContainer = document.getElementById('results');
 
-    const authParams = generateAuthParams();
-
-    try {
-        // console.log("Ts: "+authParams.ts);
-        // console.log("Hash: "+authParams.hash);
-        const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${authParams.ts}&apikey=${authParams.apikey}&hash=${authParams.hash}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        allCharacters = data.data.results;
-        displayResults(allCharacters);
-    } catch (error) {
-        console.error('Fetch error:', error);
-    }
-
-    searchInput.addEventListener('input', () => {
-        const query = searchInput.value.toLowerCase();
+    searchInput.addEventListener('input', async () => {
+        const query = searchInput.value;
         if (query.length < 3) {
             resultsContainer.innerHTML = ''; // Clear results if query is too short
             return;
         }
 
-        const filteredCharacters = allCharacters.filter(character => 
-            character.name.toLowerCase().includes(query)
-        );
-        displayResults(filteredCharacters);
+        const authParams = generateAuthParams();
+
+        try {
+            // console.log("Ts: "+authParams.ts);
+            // console.log("Hash: "+authParams.hash);
+            const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${query}&ts=${authParams.ts}&apikey=${authParams.apikey}&hash=${authParams.hash}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            displayResults(data.data.results);
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
     });
 
     function displayResults(characters) {
