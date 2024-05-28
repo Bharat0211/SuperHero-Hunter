e22447380c2bac802ebe28a6cf2a7080682c7605
Bharat Varshneya <bharatvarshneya99@gmail.com>
2024-05-28 21:47:01 +0530

    Providing Static Functionality

--- js/index.js
+++ js/index.js
@@ -1,30 +1,38 @@
 import { generateAuthParams, addToFavorites } from './common.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+let allCharacters = [];
+
+document.addEventListener('DOMContentLoaded', async () => {
     const searchInput = document.getElementById('search');
     const resultsContainer = document.getElementById('results');
 
-    searchInput.addEventListener('input', async () => {
-        const query = searchInput.value;
+    const authParams = generateAuthParams();
+
+    try {
+        // console.log("Ts: "+authParams.ts);
+        // console.log("Hash: "+authParams.hash);
+        const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${authParams.ts}&apikey=${authParams.apikey}&hash=${authParams.hash}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        allCharacters = data.data.results;
+        displayResults(allCharacters);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+
+    searchInput.addEventListener('input', () => {
+        const query = searchInput.value.toLowerCase();
         if (query.length < 3) {
             resultsContainer.innerHTML = ''; // Clear results if query is too short
             return;
         }
 
-        const authParams = generateAuthParams();
-
-        try {
-            // console.log("Ts: "+authParams.ts);
-            // console.log("Hash: "+authParams.hash);
-            const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${query}&ts=${authParams.ts}&apikey=${authParams.apikey}&hash=${authParams.hash}`);
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.json();
-            displayResults(data.data.results);
-        } catch (error) {
-            console.error('Fetch error:', error);
-        }
+        const filteredCharacters = allCharacters.filter(character => 
+            character.name.toLowerCase().includes(query)
+        );
+        displayResults(filteredCharacters);
     });
 
     function displayResults(characters) {
